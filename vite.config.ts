a7f6a339fbb1aa0d8043e7b9539f7ae255ae76bb
Lemac86 <To.Beckmann@gmx.de>
2022-10-07 10:43:59 +0000

    NewNewNewNew Build for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(),VitePWA({ registerType: 'autoUpdate', devOptions: {enabled: true} })],
   build: {outDir: "docs"},
   base: "/Vokabeltrainer/" 
 })
