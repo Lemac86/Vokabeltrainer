e926056700a3c58027bbab36f9cff9b01790c1f1
Lemac86 <To.Beckmann@gmx.de>
2022-10-07 12:39:38 +0000

    NuAberAuchEcht Build for Github Pages

--- src/API.ts
+++ src/API.ts
@@ -13,6 +13,18 @@ import {
   updateDoc, enableIndexedDbPersistence
 } from 'firebase/firestore';
 
+enableIndexedDbPersistence(getFirestore())
+.catch((err) => {
+  if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+  } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+  }
+});
 
 let toaster = createToaster({
   duration: 1500,
